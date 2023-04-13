4883756131d2c76130b381268e770035271514f0
Pouya <zarepouya7@gmail.com>
2023-04-13 14:08:34 +0300

    reform style of banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,12 +4,13 @@ function Banner() {
   return (
     <>
         <div className=' grid grid-cols-2 mx-auto'>
-        <div className='p-20'>
-            <div className='mt-10 sm:mt-30 text-left'>
+        <div className='p-10 sm:p-20'>
+            <div className='mt-10 sm:mt-52 text-left'>
                 <p className=' text-gray-500 font-thin sm:mt-20 text-xl'>Fitclub</p>
                 <h2 className='text-gray-500 text-bold sm:text-5xl text-xl mt-3'>Be Powefull, Do Sport !</h2>
-                <button className='bg-blue-500 sm:text-xl text-white p-2 rounded-md mt-5'>Exercises</button>
-                <h1 className='hidden sm:block text-300 text-gray-200 sm:opacity-50'>Exercises</h1>
+                <p className=' text-gray-500 font-thin text-sm sm:text-xl'>Get best advices for your next workout</p>
+                <button className='bg-blue-500 text-sm sm:text-md font-light text-white p-2 rounded-md mt-5'>Exercises</button>
+                <h1 className='hidden sm:block text-300 mt-20 text-gray-200 sm:opacity-50'>Exercises</h1>
             </div>   
             </div>
             <div className=' items-center text-center z-30'>
