820aa1dc67ec01b3d21e18efb3d992b2309c4af7
Pouya <zarepouya7@gmail.com>
2023-04-16 16:08:50 +0300

    add pagination

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 function ExerciseCard({exercise}) {
 const capitalizedExerciseName = exercise.name.charAt(0).toUpperCase() + exercise.name.slice(1);
+const capitalbodyPart = exercise.bodyPart.charAt(0).toUpperCase() + exercise.bodyPart.slice(1);
+const capitalTarget = exercise.target.charAt(0).toUpperCase() + exercise.target.slice(1);
   return (
     <Link to={`/exercises/${exercise.id}`}>
-        <div className='sm:p-20 p-5 bg-white'>
-            <h1 className='text-blue-400 text-center border-b-2 outline-offset-8 border-blue-300 border-opacity-70 sm:text-sm text-xs p-4'>{capitalizedExerciseName}</h1>
+        <div className='sm:p-10 p-5 bg-white hover:scale-110 transition-all duration-300'>
+            <h1 className='text-blue-400 text-center border-b-2 outline-offset-8 border-blue-300 border-opacity-70 sm:text-xs text-xs p-4 w-full'>{capitalizedExerciseName}</h1>
             <img className=' align-middle items-center' src={exercise.gifUrl} loading='lazy'/>
             <div className='flex justify-center my-5'>
-                <button className='text-white bg-blue-500 rounded-md p-2'>{exercise.target}</button>
-                <button className='text-blue-500 bg-white border-2 border-blue-500 mx-2 rounded-md p-2'>{exercise.bodyPart}</button>
+                <button className='text-white text-xs sm:text-sm w-[100px] bg-blue-500 rounded-md p-1'>{capitalTarget}</button>
+                <button className='text-blue-500 text-xs sm:text-sm w-[100px] bg-white border-2 border-blue-500 mx-2 rounded-md p-1'>{capitalbodyPart}</button>
             </div>
         </div>
     </Link>
+    
   )
 }
 
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExerciseCard from './ExerciseCard'
+import { Pagination } from '@mui/material'
 
 function Exercises({exercises,bodyPart,setexercises}) {
+  const [currentPage, setcurrentPage] = useState(1)
+  const exercisePerPage = 9;
+  const paginate = (e,value) => {
+    setcurrentPage(value);
+    window.scrollTo({top:1300,behavior:'smooth'})
+  }
+  const indexOfLastExercise = currentPage*exercisePerPage;
+  const indexOfFirstExercise = indexOfLastExercise-exercisePerPage;
+  const currentExercise = exercises.slice(indexOfFirstExercise,indexOfLastExercise)
   return (
-    <div className='grid sm:grid-cols-3 grid-cols-2 items-center text-center'>
-      {exercises.map((exercise,index) => (
+    <div className='grid sm:grid-cols-3 grid-cols-2 items-center text-center m-auto'>
+      {currentExercise.map((exercise,index) => (
         <div key={index}><ExerciseCard exercise={exercise}/></div>
       ))}
+      {
+        exercises.length>9 && (
+          <div className='flex justify-center'>
+          <Pagination
+           className='text-center items-center m-auto'
+           defaultPage={1}
+           count={Math.ceil(exercises.length/exercisePerPage)}
+           page={currentPage}
+           onChange={paginate}
+           variant="outlined" color="primary" />
+           </div>
+        )
+      }
     </div>
   )
 }
